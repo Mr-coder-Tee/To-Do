111effd63ee8e682c4d3b2a55029247b6aef4002
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2021-10-27 15:15:24 +0200

    delete design

--- Components/GlobalComponents/TodoCard/TodoCard.jsx
+++ Components/GlobalComponents/TodoCard/TodoCard.jsx
@@ -2,27 +2,43 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import style from './styles'
 import { Icon } from 'react-native-elements'
+import Swipeable from "react-native-gesture-handler/Swipeable";
+
 
 
 const TodoCard = () => {
+
+
+
+    const RightSwipe=()=>(
+        <View>
+            <Text>delete</Text>
+            <Text>edit</Text>
+        </View>
+    )
+
+
+
     return (
-        <View style={style.conatainer}>
-            <View style={style.priorityColor} />
-            <View style={{marginLeft:5}}>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <Text>Meeting</Text>
+        <Swipeable renderRightActions={RightSwipe}>
+            <View style={style.conatainer}>
+                <View style={style.priorityColor} />
+                <View style={{ marginLeft: 5 }}>
                     <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                        <Text>10:30</Text>
-                        <Icon name="check-square-o" type="font-awesome" color="green" style={{marginLeft:10}}/>
+                        <Text>Meeting</Text>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                            <Text>10:30</Text>
+                            <Icon name="check-square-o" type="font-awesome" color="green" style={{ marginLeft: 10 }} />
+                        </View>
+                    </View>
+                    <View>
+                        <Text>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, esse?
+                        </Text>
                     </View>
                 </View>
-                <View>
-                    <Text>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, esse?
-                    </Text>
-                </View>
             </View>
-        </View>
+        </Swipeable>
     )
 }
 
